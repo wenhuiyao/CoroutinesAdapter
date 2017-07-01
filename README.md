495b8c0a661f966cf7af591da63fd7203f7cf0c4
Wenhui Yao <wenhuiyao01@gmail.com>
2017-07-01 14:31:15 -0700

    Update example

--- README.md
+++ README.md
@@ -1,53 +1,83 @@
 # CoroutinesAdapter
-Adapter Kotlin coroutines for Android usage
+Adapt Kotlin [coroutines](https://github.com/Kotlin/kotlinx.coroutines) core library for Java usage.
 
+[Introduction to Kotlin coroutines](https://kotlinlang.org/docs/reference/coroutines.html)
 
-### Example
+[Examples usage of the core library](https://github.com/Kotlin/kotlinx.coroutines/blob/master/coroutines-guide.md)
+
+Unfortunately, Kotlin coroutines can only be used directly by Kotlin, so this library is to adapt Kotlin to use by Java.
 
 NOTE:
-1. Kotlin coroutine is still an experimental feature
-2. All example is written using Java 8 lambda expression
+1. Kotlin coroutines is still an experimental feature
+2. All example is written using Java 8 lambda expression, but can be use back to Java 6
+3. The library doesn't cover all the use case of coroutines
 
-##### Simple usage
-```Java
-    backgroundWork(() -> {
-            Log.d(TAG, "enter with background thread: " + getCurrentThreadName());
-            return doLengthlyWork();
-        }).onSuccess((value) -> {
-            Log.d(TAG, "onSuccess");
-            // Do work when background work done. This is running on UI thread
-            return Unit.INSTANCE;
-        }).onError(throwable -> {
-            Log.d(TAG, "onError");
-            // Do work when background work has exeception. This is running on UI thread
-            return Unit.INSTANCE;
-        }).setStartDelay(2000).start();
-```
-
-##### Transform response to different value
+##### Simple background work usage
 
 ```Java
-    background(() -> {
-            Log.d(TAG, "enter with background thread: " + getCurrentThreadName());
-            return doLengthlyWork();
-        }).transform(CONTEXT_BG, (value) -> {
-            Log.d(TAG, "enter background transformation: " + getCurrentThreadName());
-            // Transform value to a new value
-            return newValue;
-        }).onSuccess((value) -> {
-            Log.d(TAG, "onSuccess");
-            // Do work when background work done. This is running on UI thread
-            return Unit.INSTANCE;
-        }).onError(throwable -> {
-            Log.d(TAG, "onError");
-            // Do work when background work has exeception. This is running on UI thread
-            return Unit.INSTANCE;
-        }).setStartDelay(2000).start();
+Work work = background(() -> {
+        return doSomeBackgroundWork();
+    }).transform(CONTEXT_BG, (value) -> {
+        // Optionally, transform value to a new value
+        return newValue;
+    }).onSuccess((value) -> {
+        // Do work when background work done. This is running on UI thread
+        return Unit.INSTANCE;
+    }).onError(throwable -> {
+        // Do work when background work has exeception. This is running on UI thread
+        return Unit.INSTANCE;
+    }).setStartDelay(2000).start();
+
+// When activity/fragment is destroyed, the work can be canceled to avoid memory leak
+work.cancel();
+
 ```
 
-##### Specific Context when doing transformation
+##### Use Producer
+
+```Java
+Producer producer = Producers.consumeBy((Integer element) -> {
+        // do some work in the background
+        int result = element % 5;
+        return result;
+    }).filter(element -> {
+        return element % 2 == 0; // only interesting in any even numbers
+    }).operate(result -> {
+        // consume the data, e.g. saving it to database
+        return Unit.INSTANCE; // must return this
+    }).transform(CoroutineContexts.UI, element ->  {
+        // optionally, data can be consumed on UI thread
+        return "Consume " + element;
+    }).onSuccess(element -> {
+        // success callback
+        return Unit.INSTANCE;
+    }).onError(throwable -> {
+        // error callback
+        return Unit.INSTANCE;
+    }).build();
+
+    // Now, produce item to be consumed by the above code
+    producer.produce(element);
+
+    // When producer is no longer needed
+    producer.close();
+```
+
+##### Specific Context when doing operation
 ```Java
 1. CONTEXT_BG -> Doing working on background
 2. CONTEXT_UI -> Doing working on UI thread
 3. CONTEXT_NON_CANCELLABLE -> Indicate the work can't be cancelled
+```
+
+##### Usage
+```Groovy
+repositories {
+    maven { url "http://dl.bintray.com/wyao/CoroutinesAdapter" }
+}
+
+dependencies {
+    compile 'com.wenhui:coroutines-adapter:0.2.1'
+}
+
 ```
--- app/src/main/java/com/wenhui/coroutines/example/MainActivity.java
+++ app/src/main/java/com/wenhui/coroutines/example/MainActivity.java
@@ -10,7 +10,11 @@ import android.widget.Toast;
 import com.wenhui.coroutines.experimental.BackgroundWorkManager;
 import com.wenhui.coroutines.experimental.CoroutineContexts;
 import com.wenhui.coroutines.experimental.Coroutines;
+import com.wenhui.coroutines.experimental.Producer;
+import com.wenhui.coroutines.experimental.Producers;
 import kotlin.Unit;
+
+import java.util.Random;
 
 /**
  * Created by wyao on 6/24/17.
@@ -19,7 +23,8 @@ import kotlin.Unit;
 public class MainActivity extends FragmentActivity {
 
     private static final String TAG = "MainActivity";
-    private BackgroundWorkManager mJobManager = new BackgroundWorkManager();
+    private BackgroundWorkManager mWorkManager = new BackgroundWorkManager();
+    private Random mRandom = new Random(System.currentTimeMillis());
     private TextView mTextView;
 
     @Override
@@ -28,77 +33,74 @@ public class MainActivity extends FragmentActivity {
 
         setContentView(R.layout.activity_main);
 
-        View button = findViewById(R.id.button);
+        View button = findViewById(R.id.simpleBackgroundWorkButton);
         button.setOnClickListener((v) -> onClick());
+
+        View button1 = findViewById(R.id.producerWorkButton);
+        Producer<Integer> producer = producer();
+        button1.setOnClickListener(v -> {
+            int randomInt = mRandom.nextInt(100);
+            producer.produce(randomInt);
+        });
 
         mTextView = (TextView) findViewById(R.id.textView);
     }
 
     private void onClick() {
-        if (mJobManager.hasActiveWorks()) {
-            mJobManager.cancelAllWorks();
+        if (mWorkManager.hasActiveWorks()) {
+            mWorkManager.cancelAllWorks();
             mTextView.setText("Cancel current work");
             return;
         }
 
         mTextView.setText("start background work");
         Coroutines.backgroundWork(() -> {
-            Log.d(TAG, "enter with background thread: " + getCurrentThreadName());
-            final int count = 1000;
-            startBackgroundWork(count);
-            return count;
-        }).transform((count) -> {
-            Log.d(TAG, "enter background transformation: " + getCurrentThreadName());
-            return startBackgroundTransformation(count);
-        }).transform(CoroutineContexts.NON_CANCELLABLE, (count) -> {
-            Log.d(TAG, "enter non cancellable transformation: " + getCurrentThreadName());
-            return count;
-        }).transform(CoroutineContexts.UI, (count) -> {
-            Log.d(TAG, "enter UI transformation: " + getCurrentThreadName());
-            mTextView.setText("transform in UI Thread");
-            return "transformed " + (count * 2);
-        }).transform((value) -> {
-            Log.d(TAG, "enter back to background transformation: " + getCurrentThreadName());
-            startBackgroundTransformation(100);
-            return "[second transform]: " + value;
+            // simulate intensive work
+            final int sleep = 2000;
+            ThreadUtils.sleep(sleep);
+            return sleep;
+        }).transform(CoroutineContexts.BACKGROUND, data -> {
+            // Optionally, transform the data to different data
+            // and this is running in background
+            ThreadUtils.sleep(1000);
+            return "Sleep " + data + " ms";
         }).onSuccess((value) -> {
-            Log.d(TAG, "onSuccess");
-            mTextView.setText("final value =" + value);
+            // This is running on UI thread
+            mTextView.setText(value);
             return Unit.INSTANCE;
         }).onError(e -> {
             Log.d(TAG, "onError");
             Toast.makeText(this, "error: " + e.getMessage(), Toast.LENGTH_LONG).show();
             return Unit.INSTANCE;
-        }).setStartDelay(2000).manageBy(mJobManager).start();
+        }).setStartDelay(1000).manageBy(mWorkManager).start();
 
     }
 
-    private void startBackgroundWork(int count) {
-        try {
-            int sleep = count;
-            Log.d(TAG, "[with background] sleep " + sleep + "ms");
-            Thread.sleep(sleep);
-        } catch (InterruptedException ignored) {
-        }
-    }
-
-    private int startBackgroundTransformation(int count) {
-        try {
-            int sleep = count;
-            Log.d(TAG, "[transform background] sleep " + sleep + "ms");
-            Thread.sleep(sleep);
-        } catch (InterruptedException ignored) {
-        }
-        return count;
-    }
-
-    private static String getCurrentThreadName() {
-        return Thread.currentThread().getName();
+    private Producer<Integer> producer() {
+        return Producers.consumeBy((Integer element) -> {
+            // do intensive work in the background
+            ThreadUtils.sleep(1000);
+            int result = element % 5;
+            return result; // result
+        }).filter(element -> {
+            return element % 2 == 0; // only interesting in any even numbers
+        }).operate(result -> {
+            // consume the data, like saving it to database
+            ThreadUtils.sleep(200);
+            return Unit.INSTANCE; // must return this
+        }).transform(CoroutineContexts.UI, element ->  {
+            // optionally, data can be consumed on UI thread
+            return "Consume " + element;
+        }).onSuccess(element -> {
+            // callback on UI thread
+            mTextView.setText(element);
+            return Unit.INSTANCE;
+        }).manageBy(mWorkManager).build();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mJobManager.cancelAllWorks();
+        mWorkManager.cancelAllWorks();
     }
 }
